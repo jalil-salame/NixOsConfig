155f47a169835ca50ae4e84f8c743d27ffd16bb9
Jalil David Salamé Messina <jalil.salame@gmail.com>
2023-08-07 22:28:34 +0200

    fix: SImplify starship prompt

--- common/starship.nix
+++ common/starship.nix
@@ -1,20 +1,12 @@
-# let
-#   sep = (fg: bg: "[\](fg:${fg} bg:${bg})");
-#   logo = "[░▒▓](#ebdbb2)[ \ ](fg:#458588 bg:#ebdbb2)${sep "#ebdbb2" "#3c3836"}";
-#   time = "$time${sep "#3c3836" "#fe8019"}";
-#   directory = "$directory${sep "#fe8019" "#504945"}";
-#   git = "$git_branch$git_status${sep "#504945" "#3c3836"}";
-#   toolchains = "$c$cmake$go$haskell$java$kotlin[\ ](fg:#3c3836)";
-# in
 {
-  format = "[\](fg:#458588)$time$all"; # "${logo}${time}${directory}${git}${toolchains}\n$cmd__duration$character";
+  format = "$time$all";
   add_newline = false;
   cmd_duration = {
     min_time = 500;
     show_milliseconds = true;
   };
   time = {
-    format = "[  $time](bold yellow) ";
+    format = "[$time](bold yellow) ";
     disabled = false;
   };
   status = {
