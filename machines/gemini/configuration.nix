50f57fde641aeda333f2984e9ec5aa75b81099d0
Jalil David Salamé Messina <jalil.salame@gmail.com>
2023-09-03 23:18:27 +0200

    fix(gemini): Limit bootable configs to 3
    That way we don't fill up the boot partition with too many kernels

--- machines/gemini/configuration.nix
+++ machines/gemini/configuration.nix
@@ -16,7 +16,7 @@
 
   # Use the systemd-boot EFI boot loader.
   boot.loader.systemd-boot.enable = true;
-  boot.loader.systemd-boot.configurationLimit = 10;
+  boot.loader.systemd-boot.configurationLimit = 3;
   boot.loader.efi.canTouchEfiVariables = true;
 
   # Enable bluetooth
