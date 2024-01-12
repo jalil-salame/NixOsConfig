3c3813034c4203280339abbc5f7a525df9aaf887
Jalil David Salamé Messina <jalil.salame@gmail.com>
2024-01-12 12:17:02 +0100

    fixup! feat: Use fuzzel instaed of sway

--- home/gui/fuzzel.nix
+++ home/gui/fuzzel.nix
@@ -2,6 +2,7 @@
   inherit (config.wayland.windowManager) sway;
 in {
   programs.fuzzel.enable = true;
-  programs.fuzzel.settings.icon-theme = "Papirus-Dark";
-  programs.fuzzel.settings.terminal = sway.config.terminal;
+  programs.fuzzel.settings.main.icon-theme = "Papirus-Dark";
+  programs.fuzzel.settings.main.terminal = sway.config.terminal;
+  programs.fuzzel.settings.main.layer = "overlay";
 }
